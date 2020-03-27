ad99646ab3b021840f9fdd3977d8b9742ddaff93
Emmus Monsalve <emonsalvep38@gmail.com>
2020-03-27 01:17:53 -0500

    Add configurations to initial deploy with heroku

---
+++ server.js
@@ -0,0 +1,26 @@
+var express = require('express');
+var cors = require('cors');
+
+var app = express();
+var path = require('path');
+
+
+// set the port of our application
+// process.env.PORT lets the port be set by Heroku
+var port = process.env.PORT || 8080;
+
+//cross origin request
+app.use(cors());
+
+app.get('/', function(req, res) {
+    res.sendFile(path.join(__dirname + '/public/views/index.html'));
+});
+
+app.listen(port, function() {
+    console.log('Our app is running on http://localhost:' + port);
+});
+
+module.exports = {
+    app,
+    express
+};
